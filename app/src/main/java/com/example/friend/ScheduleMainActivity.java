1da613b472aa3f9285499b55b2d06c09f7844acd
pingkka <suyun1999@naver.com>
2020-09-02 20:11:10 +0900

    try add schedule list, check my git repo we_meet_java, scheduleAppServer.java modify,,,,,

--- app/src/main/java/com/example/friend/ScheduleMainActivity.java
+++ app/src/main/java/com/example/friend/ScheduleMainActivity.java
@@ -39,7 +39,7 @@ public class ScheduleMainActivity extends AppCompatActivity {
     private ActivityScheduleMainBinding activityScheduleMainBinding;
     private ArrayList<Schedule> schedules;
     private ScheduleAdapter scheduleAdapter;
-    String[] sche_list;
+    String[] schedule_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +49,7 @@ public class ScheduleMainActivity extends AppCompatActivity {
 
         try {
             String result = new CustomTask().execute("id","id","name","loadSche").get();
-            sche_list = result.split("\t");
+            schedule_list = result.split("\t");
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -63,7 +63,7 @@ public class ScheduleMainActivity extends AppCompatActivity {
         scheduleAdapter = new ScheduleAdapter(ScheduleMainActivity.this, schedules);
         activityScheduleMainBinding.scheduleList.setAdapter(scheduleAdapter);
 
-        schedules.add(0, new Schedule(sche_list[0]));
+        schedules.add(0, new Schedule(schedule_list[0]));
         scheduleAdapter.notifyItemInserted(0);
 
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(activityScheduleMainBinding.scheduleList.getContext(), linearLayoutManager.getOrientation());
